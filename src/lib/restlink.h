11cb503f98e7174ccb6c72673a22677b23e0ff43
commander15 <amadoubenjamain@gmail.com>
2025-01-05 09:09:54 +0100

    Create restlink.h

---
+++ src/lib/restlink.h
@@ -0,0 +1,12 @@
+#ifndef RESTLINK_H
+#define RESTLINK_H
+
+#include <RestLink/api.h>
+#include <RestLink/request.h>
+#include <RestLink/pathparameter.h>
+#include <RestLink/queryparameter.h>
+#include <RestLink/header.h>
+#include <RestLink/body.h>
+#include <RestLink/response.h>
+
+#endif // RESTLINK_H
