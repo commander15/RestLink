30e1ad9dab66d91f0769dd9df51a02ef41acba94
commander15 <amadoubenjamain@gmail.com>
2025-04-03 22:11:59 +0100

    Fixed Excel plugin identification bug

--- src/excel/excelplugin.h
+++ src/excel/excelplugin.h
@@ -8,7 +8,7 @@ namespace RestLink {
 class ExcelPlugin : public Plugin
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID RESTLINK_PLUGIN_IID)
+    Q_PLUGIN_METADATA(IID RESTLINK_PLUGIN_IID FILE "metadata.json")
 
 public:
     explicit ExcelPlugin(QObject *parent = nullptr);
