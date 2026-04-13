fadca071ff2b4bc4c47c2e9fa78233877be654e8
commander15 <amadoubenjamain@gmail.com>
2026-04-13 09:30:12 +0100

    Marked all properties of Api as 'final'

--- src/lib/api.h
+++ src/lib/api.h
@@ -14,12 +14,12 @@ class ApiPrivate;
 class RESTLINK_EXPORT Api : public ApiBase
 {
     Q_OBJECT
-    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
-    Q_PROPERTY(QString version READ versionString WRITE setVersionString NOTIFY versionChanged)
-    Q_PROPERTY(QUrl url READ url WRITE setUrl NOTIFY urlChanged)
-    Q_PROPERTY(QLocale locale READ locale WRITE setLocale NOTIFY localeChanged)
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged FINAL)
+    Q_PROPERTY(QString version READ versionString WRITE setVersionString NOTIFY versionChanged FINAL)
+    Q_PROPERTY(QUrl url READ url WRITE setUrl NOTIFY urlChanged FINAL)
+    Q_PROPERTY(QLocale locale READ locale WRITE setLocale NOTIFY localeChanged FINAL)
     Q_PROPERTY(QString bearerToken READ bearerToken WRITE setBearerToken NOTIFY bearerTokenChanged FINAL)
-    Q_PROPERTY(QString userAgent READ userAgent WRITE setUserAgent NOTIFY userAgentChanged)
+    Q_PROPERTY(QString userAgent READ userAgent WRITE setUserAgent NOTIFY userAgentChanged FINAL)
 
 public:
     explicit Api(QObject *parent = nullptr);
